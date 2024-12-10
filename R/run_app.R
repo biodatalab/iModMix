7e591e2eecc397e72dcbf03a578e49384a1ee432
IsisNarvaez <34428713+IsisNarvaez@users.noreply.github.com>
2024-12-10 17:08:30 -0500

    Add files via upload
    Change from csv to R files

--- R/run_app.R
+++ R/run_app.R
@@ -8,39 +8,51 @@
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 run_app <- function(
-  onStart = NULL,
-  options = list(),
-  enableBookmarking = NULL,
-  uiPattern = "/",
-  ...
+    onStart = NULL,
+    options = list(),
+    enableBookmarking = NULL,
+    uiPattern = "/",
+    ...
 ) {
+  # ccRCC4_Data
   demo_data_path <- "inst/Example_data/ccRCC4_Data"
-  file_path_metab_exp <- file.path(demo_data_path, "Metab_exp.csv")
-  file_path_metab_annot <- file.path(demo_data_path, "Metab_annot.csv")
-  file_path_RNA_exp <- file.path(demo_data_path, "RNA_exp.csv")
-  file_path_RNA_annot <- file.path(demo_data_path, "RNA_annot.csv")
-  file_path_metadata <- file.path(demo_data_path, "Metadata.csv")
+  file_path_metab_exp <- file.path(demo_data_path, "Metab_exp.rds")
+  file_path_metab_annot <- file.path(demo_data_path, "Metab_annot.rds")
+  file_path_RNA_exp <- file.path(demo_data_path, "RNA_exp.rds")
+  file_path_RNA_annot <- file.path(demo_data_path, "RNA_annot.rds")
+  file_path_metadata <- file.path(demo_data_path, "Metadata.rds")
+  file_path_partial_cor_metab <- file.path(demo_data_path, "PartialCorMetabolites.rds")
+  file_path_partial_cor_RNA <- file.path(demo_data_path, "PartialCorGenes.rds")
+  file_path_enrichment <- file.path(demo_data_path, "Enrichment.rds")
 
-  precargados_metab_exp <<- read.csv(file_path_metab_exp)
-  precargados_metab_annot <<- read.csv(file_path_metab_annot)
-  precargados_RNA_exp <<- read.csv(file_path_RNA_exp)
-  precargados_RNA_annot <<- read.csv(file_path_RNA_annot)
-  precargados_metadata <<- read.csv(file_path_metadata)
+  precargados_metab_exp <<- readRDS(file_path_metab_exp)
+  precargados_metab_annot <<- readRDS(file_path_metab_annot)
+  precargados_RNA_exp <<- readRDS(file_path_RNA_exp)
+  precargados_RNA_annot <<- readRDS(file_path_RNA_annot)
+  precargados_metadata <<- readRDS(file_path_metadata)
+  precargados_partial_cor_metab <<- readRDS(file_path_partial_cor_metab)
+  precargados_partial_cor_RNA <<- readRDS(file_path_partial_cor_RNA)
+  precargados_enrichment <<- readRDS(file_path_enrichment)
 
-  temp_dir <- tempdir()
-  unzip(file.path(demo_data_path, "PartialCorMetabolites.csv.zip"), exdir = temp_dir)
-  file_path_partial_cor_metab <- file.path(temp_dir, "PartialCorMetabolites.csv")
-  precargados_partial_cor_metab <<- read.csv(file_path_partial_cor_metab, header = TRUE, row.names = 1, check.names = FALSE)
-  precargados_partial_cor_metab <<- as.matrix(precargados_partial_cor_metab)
+  # FloresData_K_TK
+  demo_data_pathAll <- "inst/Example_data/FloresData_K_TK"
+  file_path_metab_expAll <- file.path(demo_data_pathAll, "Metab_exp.rds")
+  file_path_metab_annotAll <- file.path(demo_data_pathAll, "Metab_annot.rds")
+  file_path_RNA_expAll <- file.path(demo_data_pathAll, "Prot_exp.rds")
+  file_path_RNA_annotAll <- file.path(demo_data_pathAll, "Prot_annot.rds")
+  file_path_metadataAll <- file.path(demo_data_pathAll, "Metadata.rds")
+  file_path_partial_cor_metabAll <- file.path(demo_data_pathAll, "PartialCorMetabolites.rds")
+  file_path_partial_cor_RNAAll <- file.path(demo_data_pathAll, "PartialCorProt.rds")
+  file_path_enrichmentAll <- file.path(demo_data_pathAll, "EnrichmentMouse.rds")
 
-  unzip(file.path(demo_data_path, "PartialCorGenes.csv.zip"), exdir = temp_dir)
-  file_path_partial_cor_RNA <- file.path(temp_dir, "PartialCorGenes.csv")
-  precargados_partial_cor_RNA <<- read.csv(file_path_partial_cor_RNA, header = TRUE, row.names = 1, check.names = FALSE)
-  precargados_partial_cor_RNA <<- as.matrix(precargados_partial_cor_RNA)
-
-  file_path_enrichment <- file.path(demo_data_path, "Enrichment.csv")
-  precargados_enrichment <<- read.csv(file_path_enrichment, header = TRUE, row.names = 1, check.names = FALSE)
-
+  precargados_metab_expAll <<- readRDS(file_path_metab_expAll)
+  precargados_metab_annotAll <<- readRDS(file_path_metab_annotAll)
+  precargados_RNA_expAll <<- readRDS(file_path_RNA_expAll)
+  precargados_RNA_annotAll <<- readRDS(file_path_RNA_annotAll)
+  precargados_metadataAll <<- readRDS(file_path_metadataAll)
+  precargados_partial_cor_metabAll <<- readRDS(file_path_partial_cor_metabAll)
+  precargados_partial_cor_RNAAll <<- readRDS(file_path_partial_cor_RNAAll)
+  precargados_enrichmentAll <<- readRDS(file_path_enrichmentAll)
 
   # Definir funciones para cargar los datos
   load_metab_exp <<- function() {
@@ -68,37 +80,6 @@ run_app <- function(
     return(precargados_enrichment)
   }
 
-  ### data Flores
-
-  demo_data_pathAll <- "inst/Example_data/FloresData_K_TK"
-  file_path_metab_expAll <- file.path(demo_data_pathAll, "Metab_exp.csv")
-  file_path_metab_annotAll <- file.path(demo_data_pathAll, "Metab_annot.csv")
-  file_path_RNA_expAll <- file.path(demo_data_pathAll, "Prot_exp.csv")
-  file_path_RNA_annotAll <- file.path(demo_data_pathAll, "Prot_annot.csv")
-  file_path_metadataAll <- file.path(demo_data_pathAll, "Metadata.csv")
-
-  precargados_metab_expAll <<- read.csv(file_path_metab_expAll)
-  precargados_metab_annotAll <<- read.csv(file_path_metab_annotAll)
-  precargados_RNA_expAll <<- read.csv(file_path_RNA_expAll)
-  precargados_RNA_annotAll <<- read.csv(file_path_RNA_annotAll)
-  precargados_metadataAll <<- read.csv(file_path_metadataAll)
-
-  temp_dirAll <- tempdir()
-  unzip(file.path(demo_data_pathAll, "PartialCorMetabolites.csv.zip"), exdir = temp_dirAll)
-  file_path_partial_cor_metabAll <- file.path(temp_dirAll, "PartialCorMetabolites.csv")
-  precargados_partial_cor_metabAll <<- read.csv(file_path_partial_cor_metabAll, header = TRUE, row.names = 1, check.names = FALSE)
-  precargados_partial_cor_metabAll <<- as.matrix(precargados_partial_cor_metabAll)
-
-  unzip(file.path(demo_data_pathAll, "PartialCorProt.csv.zip"), exdir = temp_dirAll)
-  file_path_partial_cor_RNAAll <- file.path(temp_dirAll, "PartialCorProt.csv")
-  precargados_partial_cor_RNAAll <<- read.csv(file_path_partial_cor_RNAAll, header = TRUE, row.names = 1, check.names = FALSE)
-  precargados_partial_cor_RNAAll <<- as.matrix(precargados_partial_cor_RNAAll)
-
-  file_path_enrichmentAll <- file.path(demo_data_pathAll, "EnrichmentMouse.csv")
-  precargados_enrichmentAll <<- read.csv(file_path_enrichmentAll, header = TRUE, row.names = 1, check.names = FALSE)
-
-
-  # Definir funciones para cargar los datos
   load_metab_expAll <<- function() {
     return(precargados_metab_expAll)
   }
@@ -137,3 +118,4 @@ run_app <- function(
     golem_opts = list(...)
   )
 }
+
